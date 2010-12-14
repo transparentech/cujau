247d05d465a43eea465dd448bf5233128c95769e
Nicholas Rahn <nick@transparentech.com>
2010-12-14 11:52:16 +0100

    Remove unused commented code.

--- src/main/java/org/cujau/utils/PlatformOpen.java
+++ src/main/java/org/cujau/utils/PlatformOpen.java
@@ -55,41 +55,6 @@ public class PlatformOpen {
         }
     }
 
-//    public static void openSimple( String urlOrPath ) {
-//        String osName = System.getProperty( "os.name" );
-//        try {
-//            if ( osName.startsWith( "Mac OS" ) ) {
-//                // Class<?> fileMgr = Class.forName( "com.apple.eio.FileManager" );
-//                // Method openURL = fileMgr.getDeclaredMethod( "openURL", new Class[] { String.class
-//                // } );
-//                // openURL.invoke( null, new Object[] { url } );
-//                Runtime.getRuntime().exec( "open " + urlOrPath );
-//            } else if ( osName.startsWith( "Windows" ) ) {
-//                Runtime.getRuntime().exec( "rundll32 url.dll,FileProtocolHandler " + urlOrPath );
-//            } else { // assume Unix or Linux
-//                // String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla",
-//                // "netscape" };
-//                // String browser = null;
-//                // for ( int count = 0; count < browsers.length && browser == null; count++ ) {
-//                // if ( Runtime.getRuntime().exec( new String[] { "which", browsers[count] }
-//                // ).waitFor()
-//                // == 0 ) {
-//                // browser = browsers[count];
-//                // }
-//                // }
-//                // if ( browser == null ) {
-//                // throw new IllegalStateException( "Could not find web browser on '" + osName + "'"
-//                // );
-//                // } else {
-//                // Runtime.getRuntime().exec( new String[] { browser, url } );
-//                // }
-//                Runtime.getRuntime().exec( "xdg-open " + urlOrPath );
-//            }
-//        } catch ( Exception e ) {
-//            throw new IllegalStateException( "Unable to open URL or Path: " + urlOrPath );
-//        }
-//    }
-
     /**
      * Open a string URL
      * 
