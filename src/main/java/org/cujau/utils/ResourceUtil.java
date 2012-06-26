9b1fca7d9615f9dc742ae137742e432e34fe4236
Nicholas Rahn <nick@transparentech.com>
2012-06-26 10:32:49 +0200

    Refactoring of stream/resource methods.

--- src/main/java/org/cujau/utils/ResourceUtil.java
+++ src/main/java/org/cujau/utils/ResourceUtil.java
@@ -1,10 +1,8 @@
 package org.cujau.utils;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,8 +14,6 @@ public class ResourceUtil {
 
     private static final Logger LOG = LoggerFactory.getLogger( ResourceUtil.class );
 
-    private static final int BUFFER_SIZE = 4096;
-    
     /**
      * Return the requested resource as a String.
      * <p>
@@ -37,30 +33,7 @@ public class ResourceUtil {
             LOG.info( "Can't find the requested resource: {}", resourceName );
             return null;
         }
-        return getStreamAsString( is );
-    }
-
-    /**
-     * Return the given InputStream contents as a String.
-     * <p>
-     * This method assumes that the InputStream contents is a text encoded in UTF-8.
-     * </p>
-     * 
-     * @param is
-     *            The InputStream whose contents is converted to a String.
-     * @return The String.
-     * @throws IOException
-     */
-    public static String getStreamAsString( InputStream is )
-            throws IOException {
-        InputStreamReader reader = new InputStreamReader( is, "UTF-8" );
-        StringBuilder b = new StringBuilder();
-        char[] buffer = new char[BUFFER_SIZE];
-        int read;
-        while ( ( read = reader.read( buffer ) ) != -1 ) {
-            b.append( buffer, 0, read );
-        }
-        return b.toString();
+        return StreamUtil.getStreamAsString( is );
     }
 
     /**
@@ -79,24 +52,7 @@ public class ResourceUtil {
             LOG.info( "Can't find the requested resource: {}", resourceName );
             return null;
         }
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( BUFFER_SIZE );
-        byte[] bytes = new byte[BUFFER_SIZE];
-
-        // Read bytes from the input stream in bytes.length-sized chunks and write
-        // them into the output stream
-        int readBytes;
-        while ( ( readBytes = inputStream.read( bytes ) ) > 0 ) {
-            outputStream.write( bytes, 0, readBytes );
-        }
-
-        // Convert the contents of the output stream into a byte array
-        byte[] byteData = outputStream.toByteArray();
-
-        // Close the streams
-        inputStream.close();
-        outputStream.close();
-
-        return byteData;
+        return StreamUtil.getStreamBytes( inputStream );
     }
 
     /**
--- src/main/java/org/cujau/utils/StreamUtil.java
+++ src/main/java/org/cujau/utils/StreamUtil.java
@@ -1,16 +1,20 @@
 package org.cujau.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 
 public class StreamUtil {
 
+    static final int BUFFER_SIZE = 8192;
+
     public static void copyReader( Reader reader, Writer writer )
             throws IOException {
-        copyReader( reader, writer, 8192 );
+        copyReader( reader, writer, BUFFER_SIZE );
     }
 
     public static void copyReader( Reader reader, Writer writer, int bufferSize )
@@ -24,16 +28,58 @@ public class StreamUtil {
 
     public static void streamCopy( InputStream inputStream, OutputStream outputStream )
             throws IOException {
-        streamCopy( inputStream, outputStream, 8192 );
+        streamCopy( inputStream, outputStream, BUFFER_SIZE );
     }
 
     public static void streamCopy( InputStream inputStream, OutputStream outputStream, int bufferSize )
             throws IOException {
-        int read = -1;
         byte[] readBuffer = new byte[bufferSize];
+        
+        // Read bytes from the input stream in bufferSize chunks and write
+        // them into the output stream.
+        int read = -1;
         while ( ( read = inputStream.read( readBuffer, 0, bufferSize ) ) != -1 ) {
             outputStream.write( readBuffer, 0, read );
         }
+    }
+
+    public static byte[] getStreamBytes( InputStream inputStream )
+            throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( BUFFER_SIZE );
+        streamCopy( inputStream, outputStream );
+
+        // Convert the contents of the output stream into a byte array.
+        byte[] byteData = outputStream.toByteArray();
+
+        // Close the streams
+        inputStream.close();
+        outputStream.close();
+
+        return byteData;
+
+    }
+
+    /**
+     * Return the given InputStream contents as a String.
+     * <p>
+     * This method assumes that the InputStream contents is a text encoded in UTF-8.
+     * </p>
+     * 
+     * @param is
+     *            The InputStream whose contents is converted to a String.
+     * @return The String.
+     * @throws IOException
+     */
+    public static String getStreamAsString( InputStream is )
+            throws IOException {
+        InputStreamReader reader = new InputStreamReader( is, "UTF-8" );
+        StringBuilder b = new StringBuilder();
+        char[] buffer = new char[BUFFER_SIZE];
+        int read;
+        while ( ( read = reader.read( buffer ) ) != -1 ) {
+            b.append( buffer, 0, read );
+        }
+        return b.toString();
     }
 
 }
