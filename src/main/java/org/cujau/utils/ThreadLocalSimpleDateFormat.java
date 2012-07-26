450a04961ce9fc244406e989302ec9f1440c460e
Nicholas Rahn <nick@transparentech.com>
2012-07-26 17:27:47 +0200

    Extra consctructor for threadlocal SDF.

--- src/main/java/org/cujau/utils/ThreadLocalSimpleDateFormat.java
+++ src/main/java/org/cujau/utils/ThreadLocalSimpleDateFormat.java
@@ -1,6 +1,7 @@
 package org.cujau.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 /**
  * Utility for using <tt>java.text.SimpleDateFormat</tt> in a thread-safe way.
@@ -24,12 +25,14 @@ import java.text.SimpleDateFormat;
 public class ThreadLocalSimpleDateFormat extends ThreadLocal<SimpleDateFormat> {
 
     private final String format;
-
+    private final TimeZone zone;
+    
     /**
      * Constructs SimpleDateFormat instances using the default date format.
      */
     public ThreadLocalSimpleDateFormat() {
         this.format = null;
+        this.zone = null;
     }
 
     /**
@@ -41,11 +44,21 @@ public class ThreadLocalSimpleDateFormat extends ThreadLocal<SimpleDateFormat> {
      */
     public ThreadLocalSimpleDateFormat( String format ) {
         this.format = format;
+        this.zone = null;
+    }
+    
+    public ThreadLocalSimpleDateFormat( String format, TimeZone zone ) {
+        this.format = format;
+        this.zone = zone;
     }
 
     @Override
     protected SimpleDateFormat initialValue() {
-        return new SimpleDateFormat( format );
+        SimpleDateFormat sdf = new SimpleDateFormat( format );
+        if ( zone != null ) {
+            sdf.setTimeZone( zone );
+        }
+        return sdf;
     }
 
 }
