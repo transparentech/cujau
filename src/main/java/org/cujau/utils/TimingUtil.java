39f0b2d88d4adf01c920b3308de72c2a89da50ea
Nicholas Rahn <nick@transparentech.com>
2013-05-27 14:53:14 +0200

    Add extra methods for displaying a millisecond value as a time unit string.

--- src/main/java/org/cujau/utils/TimingUtil.java
+++ src/main/java/org/cujau/utils/TimingUtil.java
@@ -1,5 +1,6 @@
 package org.cujau.utils;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -79,5 +80,138 @@ public class TimingUtil {
         laps.remove( name );
         lapCount.remove( name );
         return lap;
+    }
+
+    /**
+     * Get the formatted String representation of the given lap value.
+     * 
+     * @param lapTime
+     *            The lap time in milliseconds.
+     * @return A String representing the lap value formatted with {@link #formatTimeInMillis(long)}.
+     */
+    public String formatLap( long lapTime ) {
+        return formatTimeInMillis( lapTime );
+    }
+
+    /**
+     * Convert the given time in milliseconds to a String with the format:
+     * 
+     * <pre>
+     * NNd, NNh, NNm, NNs, NNms
+     * </pre>
+     * 
+     * Where "NN" is the value and
+     * <ul>
+     * <li>d = days</li>
+     * <li>h = hours</li>
+     * <li>m = minutes</li>
+     * <li>s = seconds</li>
+     * <li>ms = milliseconds</li>
+     * </ul>
+     * Values starting from the left of the formatted string will be omitted if they are O until the
+     * first non-zero value appears.
+     * 
+     * @param time
+     *            The time in milliseconds to format.
+     * @return A String with the formatted time.
+     */
+    public static String formatTimeInMillis( long time ) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis( time );
+        c.add( Calendar.HOUR_OF_DAY, -1 );
+        String ret = "";
+        ret = format( ret, "d", c, Calendar.DAY_OF_YEAR );
+        ret = format( ret, "h", c, Calendar.HOUR_OF_DAY );
+        ret = format( ret, "m", c, Calendar.MINUTE );
+        ret = format( ret, "s", c, Calendar.SECOND );
+        ret = format( ret, "ms", c, Calendar.MILLISECOND );
+        return ret;
+    }
+
+    /**
+     * Convert the input String into a millisecond value. The input must have the form:
+     * <ul>
+     * <li>X milliseconds = "Xms"</li>
+     * <li>X seconds = "Xs"</li>
+     * <li>X minutes = "Xm"</li>
+     * <li>X hours = "Xh"</li>
+     * <li>X days = "Xd"</li>
+     * <li>X weeks = "Xw"</li>
+     * </ul>
+     * For example: "1w 2d 3h 4m 5s 678ms".
+     * 
+     * @param stringValue
+     * @return Value in milliseconds
+     */
+    public static long stringToMillis( String stringValue ) {
+
+        if ( stringValue == null ) {
+            return 0;
+        }
+
+        stringValue = stringValue.trim();
+        if ( stringValue.endsWith( "ms" ) ) {
+            // Milliseconds
+            return Long.parseLong( stringValue.trim().substring( 0, stringValue.length() - 2 ) );
+        }
+
+        // Setup in millis
+        char unit = stringValue.trim().charAt( stringValue.length() - 1 );
+        long wert = Long.parseLong( stringValue.trim().substring( 0, stringValue.length() - 1 ) );
+
+        switch ( unit ) {
+        case 's':
+            // second = 1000 millis
+            return wert * 1000;
+        case 'm':
+            // minute = 60 seconds * 1000 millis
+            return wert * 60 * 1000;
+        case 'h':
+            // hour = 60 minutes * 60 seconds * 1000 millis
+            return wert * 60 * 60 * 1000;
+        case 'd':
+            // day = 24 hours * 60 minutes * 60 seconds * 1000 millis
+            return wert * 24 * 60 * 60 * 1000;
+        case 'w':
+            // week = 7 days * 24 hours * 60 minutes * 60 seconds * 1000
+            // millis
+            return wert * 7 * 24 * 60 * 60 * 1000;
+        default:
+            throw new IllegalArgumentException( "Invalid timeunit for interval (" + unit + ")" );
+        }
+    }
+
+    /**
+     * Format the {@link java.util.Calendar} field selected with the <tt>field</tt> parameter and
+     * append it to the given <tt>ret</tt> String. Nothing will be added to the String if the value
+     * is at the field minimum value.
+     * 
+     * @param ret
+     *            The String to which the formatted value (if any) will be added.
+     * @param name
+     *            The suffix String to append if the field value is greater then the field minimum.
+     * @param c
+     *            The Calendar containing the values.
+     * @param field
+     *            The selected Calendar field.
+     * @return The String with the formatted field value appended.
+     */
+    private static String format( String ret, String name, Calendar c, int field ) {
+        int tmp = c.get( field );
+        if ( tmp == c.getActualMinimum( field ) ) {
+            return ret;
+        }
+        if ( ret.equals( "" ) ) {
+            if ( field == Calendar.DAY_OF_YEAR ) {
+                // DAY_OF_YEAR is special as the minimum value is 1. So if we
+                // are here, the day is
+                // not the minimum (1). However, this means that we need to
+                // subtract 1 from the day
+                // to have an accurate day count.
+                return ( tmp - 1 ) + name;
+            }
+            return tmp + name;
+        }
+        return ret + ", " + tmp + name;
     }
 }
