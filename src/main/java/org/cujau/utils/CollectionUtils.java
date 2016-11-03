b3ee0a37783088dd7d1e85d97185e37c48d8e0b3
Nicholas Rahn <nick@transparentech.com>
2016-11-03 22:13:58 +0100

    1.1.12 -Add CollectionUtils.

---
+++ src/main/java/org/cujau/utils/CollectionUtils.java
@@ -0,0 +1,22 @@
+package org.cujau.utils;
+
+import java.util.List;
+
+/**
+ * Utility methods for working with Collections (List, Iterable, Set, etc).
+ */
+public class CollectionUtils {
+    public static <E> E getFirst( List<E> list ) {
+        if ( list != null && !list.isEmpty() ) {
+            return list.get( 0 );
+        }
+        return null;
+    }
+
+    public static <E> E getLast( List<E> list ) {
+        if ( list != null && !list.isEmpty() ) {
+            return list.get( list.size() - 1 );
+        }
+        return null;
+    }
+}
