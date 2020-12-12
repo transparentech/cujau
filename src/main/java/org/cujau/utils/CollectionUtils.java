8853a7be4301d11d7311dd6b5dfd402f86d04453
Nicholas Rahn <nick@transparentech.com>
2020-12-12 16:41:22 -0600

    New isEmpty method for CollectionUtil.

--- src/main/java/org/cujau/utils/CollectionUtils.java
+++ src/main/java/org/cujau/utils/CollectionUtils.java
@@ -1,22 +1,27 @@
 package org.cujau.utils;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
  * Utility methods for working with Collections (List, Iterable, Set, etc).
  */
 public class CollectionUtils {
-    public static <E> E getFirst( List<E> list ) {
-        if ( list != null && !list.isEmpty() ) {
-            return list.get( 0 );
+    public static <E> E getFirst(List<E> list) {
+        if (list != null && !list.isEmpty()) {
+            return list.get(0);
         }
         return null;
     }
 
-    public static <E> E getLast( List<E> list ) {
-        if ( list != null && !list.isEmpty() ) {
-            return list.get( list.size() - 1 );
+    public static <E> E getLast(List<E> list) {
+        if (list != null && !list.isEmpty()) {
+            return list.get(list.size() - 1);
         }
         return null;
+    }
+
+    public static boolean isEmpty(Collection<?> list) {
+        return (list == null || list.isEmpty());
     }
 }
